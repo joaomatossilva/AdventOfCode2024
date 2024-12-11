ea114dc30c7e4c378436499dffdae0a4c89439e7
João Matos Silva <joao@kspace.pt>
2024-12-11 15:31:12 +0000

    Day 11

---
+++ Puzzle21/Program.cs
@@ -0,0 +1,107 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+string input = @"872027 227 18 9760 0 4 67716 9245696";
+
+var numbers = GetNumbers().Matches(input);
+int stoneIndex = numbers.Count - 1;
+
+Stone stoneTail = new Stone { Number = long.Parse(numbers[stoneIndex].Value)};
+Stone stoneHead = stoneTail;
+int count = numbers.Count;
+var stopwatch = Stopwatch.StartNew();
+
+while (stoneIndex > 0)
+{
+    stoneIndex--;
+    stoneHead = new Stone { Number = long.Parse(numbers[stoneIndex].Value), Next = stoneHead};
+}
+
+Print(-1);
+for (int i = 0; i < 25; i++)
+{
+    Blink();
+    Print(i);
+}
+
+void Blink()
+{
+    var stone = stoneHead;
+    do
+    {
+        Process();
+        stone = stone.Next;
+    } while (stone != null);
+    
+    void Process()
+    {
+        if (stone.Number == 0)
+        {
+            stone.Number = 1;
+            return;
+        }
+
+        var fullLength = GetLenght(stone.Number);
+        if (fullLength % 2 == 0)
+        {
+            var lenght = fullLength / 2;
+
+            var number = stone.Number;
+            var firstPart = number / (long)Math.Pow(10, lenght);
+            stone.Number = firstPart; //  first stone number
+            
+            var secondPart = number % (long)Math.Pow(10, lenght);
+            var newStone = new Stone { Number = secondPart, Next = stone.Next };
+            stone.Next = newStone;
+            stone = newStone;
+
+            count++;
+            
+            return;
+        }
+        
+        stone.Number *= 2024;
+    }
+}
+
+void Print(int i)
+{
+    Console.WriteLine("");
+    Console.WriteLine($"{i}=={count}== {stopwatch.Elapsed}");
+    // var stone = stoneHead;
+    // while (stone != null)
+    // {
+    //     Console.Write($" {stone.Number} ");
+    //     
+    //     stone = stone.Next;
+    // }
+}
+
+int GetLenght(long number)
+{
+    for (int i = 15; i >= 0; i--)
+    {
+        if (number / (long)Math.Pow(10, i) > 0)
+        {
+            return i + 1;
+        }
+    }
+    
+    throw new Exception();
+}
+
+partial class Program
+{
+    [GeneratedRegex("[0-9]+")]
+    private static partial Regex GetNumbers();
+}
+
+class Stone
+{
+
+    public long Number { get; set; }
+   
+    public Stone? Next { get; set; }
+};
---
+++ Puzzle22/Program.cs
@@ -0,0 +1,198 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+//872027 227 18 9760 0 4 67716 9245696
+string input = @"872027 227 18 9760 0 4 67716 9245696";
+
+var numbers = GetNumbers().Matches(input);
+int stoneIndex = numbers.Count - 1;
+
+int[] pows = new int [] {1, 10, 100, 1000, 10000, 100000, 1000000, 100000000, 1000000000};
+
+Dictionary<long, long> stones = new Dictionary<long, long>();
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    stones.Add(long.Parse(numbers[i].Value), 1);
+}
+
+var stopwatch = Stopwatch.StartNew();
+
+for (int i = 0; i < 75; i++)
+{
+
+    var blink = stones.Select(x => Process(x.Key, x.Value))
+        .SelectMany(x => x)
+        .GroupBy(x => x.Item1, x => x.Item2)
+        .ToDictionary(x => x.Key, x => x.Sum());
+
+    stones = blink;
+    
+    Console.WriteLine($"partial: {stones.Sum(x => x.Value)}");
+}
+
+var count = stones.Sum(x => x.Value);
+Console.WriteLine($"Total stones: {count}");
+Console.WriteLine($"Elapsed: {stopwatch.Elapsed}");
+
+IEnumerable<(long, long)> Process(long number, long numberCount)
+{
+    if (numberCount == 0)
+    {
+        yield break;
+    }
+    
+    if (number == 0)
+    {
+        yield return (1, numberCount);
+        yield break;
+    }
+
+    var fullLength = GetLenght(number);
+    if (fullLength % 2 == 0)
+    {
+        var pow = GetPow(fullLength / 2);
+        
+        var first = number / pow;
+        yield return (first, numberCount);
+        
+        var second = number % pow;
+        yield return (second, numberCount);
+        yield break;
+    }
+
+
+    var newNumber = number * 2024;
+    yield return (newNumber, numberCount);
+}
+
+int GetLenght(long number)
+{
+    //avoiding converting to string
+    
+    if (number < 10)
+    {
+        return 1;
+    }
+    if (number < 100)
+    {
+        return 2;
+    }
+    if (number < 1000)
+    {
+        return 3;
+    }
+    if (number < 10000)
+    {
+        return 4;
+    }
+    if (number < 100000)
+    {
+        return 5;
+    }
+    if (number < 1000000)
+    {
+        return 6;
+    }
+    if (number < 10000000)
+    {
+        return 7;
+    }
+    if (number < 100000000)
+    {
+        return 8;
+    }
+    if (number < 1000000000)
+    {
+        return 9;
+    }
+    if (number < 10000000000)
+    {
+        return 10;
+    }
+    if (number < 100000000000)
+    {
+        return 11;
+    }
+    if (number < 1000000000000)
+    {
+        return 12;
+    }
+    if (number < 10000000000000)
+    {
+        return 13;
+    }
+    if (number < 100000000000000)
+    {
+        return 14;
+    }
+    if (number < 1000000000000000)
+    {
+        return 15;
+    }
+    
+    throw new Exception();
+
+}
+
+
+int GetPow(int pow)
+{
+    //return pows[pow];
+    return (int)Math.Pow(10, pow);
+    // switch (pow)
+    // {
+    //     case 0:
+    //         return 1;
+    //     case 1:
+    //         return 10;
+    //     case 2:
+    //         return 100;
+    //     case 3:
+    //         return 1000;
+    //     case 4:
+    //         return 10000;
+    //     case 5:
+    //         return 100000;
+    //     case 6:
+    //         return 1000000;
+    //     case 7:
+    //         return 10000000;
+    //     case 8:
+    //         return 100000000;
+    //     case 9:
+    //         return 1000000000;
+    //     // case 10:
+    //     //     return 10000000000;
+    //     // case 11:
+    //     //     return 100000000000;
+    //     // case 12:
+    //     //     return 1000000000000;
+    //     // case 13:
+    //     //     return 10000000000000;
+    //     // case 14:
+    //     //     return 100000000000000;
+    //     // case 15:
+    //     //     return 1000000000000000;
+    //     default:
+    //         throw new Exception();
+    // }
+}
+
+partial class Program
+{
+    [GeneratedRegex("[0-9]+")]
+    private static partial Regex GetNumbers();
+}
+
+public static class Programex
+{
+    public static long GetOrDefault(this IDictionary<long, long> dictionary, long key)
+    {
+        return dictionary.TryGetValue(key, out var val) ? val : default;
+    }
+}
