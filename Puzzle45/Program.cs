37678c1eda719739f26973c40a085e109bfb5123
João Matos Silva <joao@kspace.pt>
2024-12-23 08:13:10 +0000

    Day 23

---
+++ Puzzle45/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var input = File.ReadAllText("input.txt")
+    .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+var map = new Dictionary<string, HashSet<string>>();
+
+foreach (var link in input)
+{
+    var parts = link.Split("-");
+    if (!map.TryAdd(parts[0], [parts[1]]))
+    {
+        map[parts[0]].Add(parts[1]);
+    }
+    
+    if (!map.TryAdd(parts[1], [parts[0]]))
+    {
+        map[parts[1]].Add(parts[0]);
+    }
+}
+
+var visited = new HashSet<(string, string, string)>();
+
+foreach (var node in map)
+{
+    if (!node.Key.StartsWith("t"))
+    {
+        continue;
+    }
+    
+    foreach (var connection in node.Value)
+    {
+        foreach (var subConnection in map[connection])
+        {
+            if (map[subConnection].Contains(node.Key))
+            {
+                var list = new List<string> { node.Key, connection, subConnection };
+                list.Sort();
+                visited.Add((list[0], list[1], list[2]));
+            }
+        }
+    }
+}
+
+Console.WriteLine(visited.Count);
---
+++ Puzzle46/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+using SharpGraph;
+
+var input = File.ReadAllText("input.txt")
+    .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+var map = new Dictionary<string, HashSet<string>>();
+List<Edge> list = new List<Edge> ();
+
+foreach (var link in input)
+{
+    var parts = link.Split("-");
+    if (!map.TryAdd(parts[0], [parts[1]]))
+    {
+        map[parts[0]].Add(parts[1]);
+    }
+    
+    if (!map.TryAdd(parts[1], [parts[0]]))
+    {
+        map[parts[1]].Add(parts[0]);
+    }
+    
+    list.Add(new Edge(parts[0], parts[1]));
+}
+
+var graph = new Graph(list);
+
+var a =graph.FindMaximalCliques()
+    .OrderByDescending(x => x.Count)
+    .First();
+
+var sorted = a.Select(x => x.ToString().Trim()).OrderBy(x => x);
+Console.WriteLine(string.Join(",", sorted));
+
